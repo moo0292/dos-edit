aee4e048ee0a4722ce3e3f805d1102c01656c655
Kittitat Rodchaidee <moo0292@kittitats-pro.attlocal.net>
2015-05-11 09:26:39 -0700

    first commit

---
+++ tests/Controllers/controllers.tests.js
@@ -0,0 +1,18 @@
+describe('Controllers', function(){
+    var scope;
+    
+    // load the controller's module
+    beforeEach(module('starter.controllers'));
+
+    beforeEach(inject(function($rootScope, $controller, $ionicModal) {
+        scope = $rootScope.$new();
+        $controller('AppCtrl', {$scope: scope, $ionicModal: $ionicModal});
+            }));
+
+    // tests start here
+    it('should have enabled friends to be true', function(){
+        expect(scope.settings.enableFriends).toEqual(true);
+        console.log(scope.settings.enableFriends);
+
+    });
+});
